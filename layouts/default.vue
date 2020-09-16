1f2aff86cc1432891f53509ae864c4a8c2e0aae9
aryankatewa <60286308+aryankatewa@users.noreply.github.com>
2020-09-16 22:36:14 +0530

    Newly Added

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <!--Your App Starts Here -->
+  <v-app id="inspire">
+<Slide Y/>   
+    <!--Header -->
+    <v-app-bar color="yellow" dark fixed app>
+      <v-toolbar-title>Aryan Katewa</v-toolbar-title>
+       <!-- Component Example -->
+      <Badge/>
+    </v-app-bar>
+<Breadcrumb/>
+<Accordion/>
+<Elevation/>
+<Elevation/>
+<Elevation/>
+<Scroll/>
+<Carousel/>
+<Grids/>
+<HorizontalCards/>
+<CustomActions/>
+<Loadingcard/>
+<Modal/>
+<Form/>
+    <!-- Body -->
+    <v-content light>
+      <nuxt /> 
+    </v-content>
+    <Elevation/>
+    <!-- Footer -->
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style> 
+  .white--text {
+    border: 1px solid;
+  }
+
+   /*
+   #inspire {
+    
+    background-color: white;
+    color: black;
+    
+  }*/
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-content>
+      Did it Work?
+    </v-content>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style> 
+</style>
